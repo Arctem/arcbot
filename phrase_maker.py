cbc5d63b23e13c5a8ba44aca828bbf3beafcf32c
Arctem <rarctem@gmail.com>
2014-06-02 20:59:41 -0500

    More phrase stuff.

--- phrase_maker.py
+++ phrase_maker.py
@@ -11,6 +11,8 @@ data = {
         'consonant' : list('bcdfghjklmnpqrstvwxyz'),
         'karst' : ['Karst', 'Flannery', 'Flanneroo'],
 
+        'num_either' : ['{number}', '{roman_number}'],
+        
         'number' : ['{digit}{number}', '{digit}'],
         'digit' : '1234567890',
 
@@ -159,23 +161,25 @@ data = {
     'movie' : {
         'template' : ['{start}: {subtitle}'],
 
-        'protaganist' : ['{Name}', '{Name} and {Name}'],
+        'protaganist' : ['{Name}', '{Name} and {Name}', '{Formal_title} {Name}'],
         'protag_group_single' : ['The Gang'],
         'protag_group_plural' : ['The Guys', 'The Girls', 'We'],
-        'name' : ['Alien', 'Andrew', 'arcbot', 'Ben', 'Benji', 'Chase', 'Chris', 'Elliot', 'Jett', 'Flannery', 'Ian', 'Jesse', 'Matt', 'Nico', 'Niko', 'Predator', 'Rob', 'Russell', 'Sarah', 'Scooby Doo', 'Stalin', 'Tyler Perry', '{Username}'],
-        'famous_person' : ['Don Knotts', 'The Harlem Globetrotters', 'Carl Sagan', 'Tyler Perry', 'Darth Vader', 'Jar-Jar Binks'],
+        'name' : ['Alien', 'Andrew', 'arcbot', 'Ben', 'Benji', 'Celine', 'Chase', 'Chris', 'Elliot', 'Jett', 'Flannery', 'Ian', 'Jesse', 'Matt', 'Nico', 'Niko', 'Predator', 'Rob', 'Russell', 'Sarah', 'Jake', 'Stalin', 'Tyler Perry', '{Username}'],
+        'famous_person' : ['Don Knotts', 'The Harlem Globetrotters', 'Carl Sagan', 'Tyler Perry', 'Darth Vader', 'Jar-Jar Binks', 'Jackie Chan', 'Dendi', 'Brock Samson', 'Hulk Hogan', 'Satan', 'Steve Buscemi', 'Sean Connery', 'Scott Chadde', 'Nicholas Cage', 'Dr. Hamdy Soliman', 'Batman', 'Spencer Brown', 'Gaben', 'The Dude'],
 
-        'start' : ['{Start} Part {gen/number}', '{Start} Part {gen/roman_number}', 'The Adventures of {protaganist}', '{protaganist}\'s {subtitle}'],
+        'start' : ['{Start} Part {gen/num_either}', 'The Adventures of {protaganist}', '{protaganist}\'s {subtitle}'],
 
         'subtitle' : ['{sub_premade}', '{Name} vs. {Name}', '{Hype_noun}', 'The Search for {Item}', '{Title}, Where\'s My {Item}?', '{Name} Joins the {Organization}', '{Name} Visits {Place}', '{Name} and {Name} Go To {Place}', 'Escape From {Place}', '{Name}\'s Last Stand', 'The Return of {Name}', '{Protag_group_single} Meets {Famous_person}', '{Protag_group_plural} Meet {Famous_person}'],
-        'sub_premade' : ['Destroy All Monsters', 'The Finale'],
+        'sub_premade' : ['Destroy All Monsters', 'The Finale', 'There and Back Again', 'No Gay Shit'],
 
         'adjective' : ['more', 'extreme', 'awesome', 'sweet', 'sick'],
-        'title' : ['Dude', 'Doc', 'Bro', 'Broseph'],
-        'organization' : ['army', 'navy', 'government', 'Ku Klux Klan', 'Klan', 'bronies'],
-        'place' : ['Washington', 'the Congo', 'Space', 'Russia', 'Prison', 'Guantanamo Bay'],
-        'item' : ['{adjective} {item}', 'money', 'dosh', 'pussy', 'robot', 'pot', 'car', 'sex', 'porn', 'booze'],
-        'hype_noun' : ['Excitement', 'Showdown', 'Arena', 'Reckoning', 'Electric Boogaloo'],
+        'title' : ['{informal_title}', '{formal_title}'],
+        'informal_title' : ['Dude', 'Doc', 'Bro', 'Broseph'],
+        'formal_title' : ['Doctor', 'Rabbi'],
+        'organization' : ['army', 'navy', 'government', 'Ku Klux Klan', 'Klan', 'bronies', 'Illuminati', 'Stone Masons', 'Na\'Vi'],
+        'place' : ['Washington', 'the Congo', 'Space', 'Russia', 'Prison', 'Guantanamo Bay', 'West Hall', 'Alta 209', 'White Castle'],
+        'item' : ['{adjective} {item}', 'money', 'dosh', 'pussy', 'dick', 'robot', 'pot', 'car', 'sex', 'porn', 'booze', 'dildo', 'fleshlight'],
+        'hype_noun' : ['Excitement', 'Showdown', 'Arena', 'Reckoning', 'Electric Boogaloo', 'XXX', 'Triple X'],
     }
 }
 
