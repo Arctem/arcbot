5ca8bb820f8713efffd26512ed08bf208af73bac
Russell White <rarctem@gmail.com>
2014-11-06 10:29:12 -0700

    Initial version of Reddit link submitting.

---
+++ links_n_shit.py
@@ -0,0 +1,79 @@
+from pprint import pprint
+import requests
+import json
+import random
+import shutil
+
+base = r'https://www.reddit.com/'
+title_list = ['I don\'t know how I feel about carjackers.',
+              'I can\'t believe how tall giraffes really are!',
+              'Do you have Die Hard™ on Blu-Ray?',
+              'Just give me two big ones, please.']
+
+def login():
+    username = 'arcbot'
+    password = open('arc_pass.txt', 'r').read().strip()
+
+    payload = {'api_type' : 'json', 'passwd' : password, 'user' : username}
+    headers = {'user-agent': 'posting script for /u/arctem\'s /u/arcbot'}
+
+    client = requests.session()
+    client.headers = headers
+    r = client.post(base + r'api/login', data=payload)
+    j = r.json()
+    client.modhash = j['json']['data']['modhash']
+    print('Logged in as {}.'.format(username))
+
+    return client
+
+def choose_link():
+    links = open('links.botdat', 'r').read().split('\n')
+    already_used = open('used_links.botdat', 'r').read().split('\n')
+    while True:
+        chosen = random.choice(links)
+        if chosen not in already_used:
+            break
+    already_used.append(chosen)
+    open('used_links.botdat', 'w').write('\n'.join(already_used).strip())
+    return chosen
+
+def submit(link, client, subreddit='WordsNShit'):
+    captcha, iden = validate_captcha(client)
+    
+    title = random.choice(title_list)
+    
+    payload = {'api_type' : 'json', 'kind' : 'link', 'sendreplies' : True,
+               'sr' : subreddit, 'title' : title, 'uh' : client.modhash,
+               'url' : link}
+
+    if captcha:
+        payload['captcha'], payload['iden'] = captcha, iden
+    
+    r = client.post(base + r'api/submit', data=payload)
+    j = r.json()
+    print(j)
+
+def validate_captcha(client):
+    r = client.get(base + r'api/needs_captcha.json')
+    if r.text == 'true':
+        r = client.post(base + r'api/new_captcha', data={'api_type' : 'json'})
+        iden = r.json()['json']['data']['iden']
+        r = client.get(base + r'captcha/{}'.format(iden))
+        with open('captcha.png', 'wb') as f:
+            for chunk in r.iter_content():
+                f.write(chunk)
+        return input('answer for captcha in captcha.png? '), iden
+    else:
+        return False
+    
+def main():
+    client = login()
+
+    link = choose_link()
+    print(link)
+    submit(link, client)
+
+if __name__ == '__main__':
+    main()
+        
+
