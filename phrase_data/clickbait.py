1068b0b702b1a3f33eb7ebeff04e620f06d7b2b6
Russell White <rarctem@gmail.com>
2014-11-20 13:55:51 -0700

    Let you know how wrong you are.

--- phrase_data/clickbait.py
+++ phrase_data/clickbait.py
@@ -1,11 +1,13 @@
 data = {
   'clickbait' : {
-        'template' : ['{crazy_event} {emote_stinger}', '{crazy_event}', '{crazy_question} {evidence_stinger}', '{crazy_question}', '{list_of_things}'],
+        'template' : ['{crazy_event} {emote_stinger}', '{crazy_event}', '{crazy_question} {evidence_stinger}', '{crazy_question}', '{list_of_things}', '{amazing_find}'],
 
         'crazy_event' : ['{Singular_person,Celebrity} {verb} {normal_action,odd_action} {singular_object,singular_animal}!'],
         'crazy_question' : ['Was {Old_celebrity} a {bad_thing,weird_thing}?', 'Is {Current_celebrity} a {bad_thing,weird_thing}?'],
 
         'list_of_things' : ['{gen/number} {things} that look like {celebrity}.', '{gen/number} {topic} tips that any {person} should know!'],
+
+        'amazing_find' : ['This {evidence} will show that you\'ve been {actioning} wrong your entire life!'],
 
         #'stinger' : [],
         'evidence_stinger' : ['this {evidence,person} {shows} {the_truth}.', 'this {evidence} will {change_state} you!', 'the answer will {change_state} you!'],
@@ -13,6 +15,7 @@ data = {
 
         'singular_person' : ['this {person}'],
         'person' : ['dad', 'mom', 'grandpa', 'grandfather', 'grandma', 'grandmother', 'politician', 'teenager', 'high schooler', 'pastor', 'rabbi', 'teacher', 'toddler'],
+        'relation' : ['boyfriend', 'girlfriend', 'fuckbuddy', 'mom', 'dad'],
         'celebrity' : ['{old_celebrity}', '{current_celebrity}'],
         'old_celebrity' : ['Gandhi', 'Hitler', 'Abraham Lincoln', 'Albert Einstein', 'Stalin'],
         'current_celebrity' : ['Jack Black', 'Obama', 'Bill Murray', 'Hannah Montana',
@@ -25,14 +28,15 @@ data = {
         'normal_action' : ['protect', 'fight', 'high five'],
         'odd_action' : ['make love to', 'castrate', 'capture', 'kiss'],
         'shows' : ['shows', 'reveals', 'knows'],
+        'actioning' : ['stealing {things}', 'cooking {things}', 'eating {things}', 'kissing {things}', 'kissing your {relation}', 'talking', 'learning', 'teaching', 'masturbating', 'hugging'],
 
         'singular_object' : ['a {object}'],
-        'object' : ['toaster', 'fire truck', 'flower', 'dildo'],
+        'object' : ['toaster', 'fire truck', 'flower', 'dildo', 'sandwich'],
         'bad_thing' : ['Nazi', 'fascist', 'Communist', 'pedophile', 'nymphomaniac'],
         'weird_thing' : ['zombie', 'alien', 'painter', 'American'],
 
         'things' : ['{objects}', '{bad_things}', '{weird_things}', '{animals}'],
-        'objects' : ['toasters', 'fire trucks', 'flowers', 'dildos'],
+        'objects' : ['toasters', 'fire trucks', 'flowers', 'dildos', 'sandwiches'],
         'bad_things' : ['Nazis', 'fascists', 'Communists', 'pedophiles', 'nymphomaniacs'],
         'weird_things' : ['zombies', 'aliens', 'painters', 'Americans'],
 
