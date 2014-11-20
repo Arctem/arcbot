5f745fd85d0f4581103e08ef9a8c8b4f497b8726
Russell White <rarctem@gmail.com>
2014-11-20 13:38:56 -0700

    Add more clickbait phrases.

--- phrase_data/clickbait.py
+++ phrase_data/clickbait.py
@@ -1,19 +1,21 @@
 data = {
   'clickbait' : {
-        'template' : ['{crazy_event} {emote_stinger}', '{crazy_event}', '{crazy_question} {evidence_stinger}', '{crazy_question}'],
+        'template' : ['{crazy_event} {emote_stinger}', '{crazy_event}', '{crazy_question} {evidence_stinger}', '{crazy_question}', '{list_of_things}'],
 
-        'crazy_event' : ['{Singular_person,Celebrity} {verb} {normal_action,odd_action} {singular_thing,singular_animal}!'],
+        'crazy_event' : ['{Singular_person,Celebrity} {verb} {normal_action,odd_action} {singular_object,singular_animal}!'],
         'crazy_question' : ['Was {Old_celebrity} a {bad_thing,weird_thing}?', 'Is {Current_celebrity} a {bad_thing,weird_thing}?'],
+
+        'list_of_things' : ['{gen/number} {things} that look like {celebrity}.', '{gen/number} {topic} tips that any {person} should know!'],
 
         #'stinger' : [],
         'evidence_stinger' : ['this {evidence,person} {shows} {the_truth}.', 'this {evidence} will {change_state} you!', 'the answer will {change_state} you!'],
         'emote_stinger' : ['first you\'ll {emote,be_state}, then you\'ll {emote,be_state}!', 'You won\'t believe what happens next.'],
 
         'singular_person' : ['this {person}'],
-        'person' : ['dad', 'mom', 'grandpa', 'grandfather', 'grandma', 'grandmother', 'politician', 'teenager', 'high schooler', 'pastor', 'rabbi', 'teacher'],
+        'person' : ['dad', 'mom', 'grandpa', 'grandfather', 'grandma', 'grandmother', 'politician', 'teenager', 'high schooler', 'pastor', 'rabbi', 'teacher', 'toddler'],
         'celebrity' : ['{old_celebrity}', '{current_celebrity}'],
         'old_celebrity' : ['Gandhi', 'Hitler', 'Abraham Lincoln', 'Albert Einstein', 'Stalin'],
-        'current_celebrity' : ['Jack Black', 'Obama', 'Bill Murray',
+        'current_celebrity' : ['Jack Black', 'Obama', 'Bill Murray', 'Hannah Montana',
             'Mittens the Cat', 'Hamdy', '{gen/karst}', '{Username}'],
 
         'evidence' : ['video', 'website'],
@@ -24,16 +26,25 @@ data = {
         'odd_action' : ['make love to', 'castrate', 'capture', 'kiss'],
         'shows' : ['shows', 'reveals', 'knows'],
 
-        'singular_thing' : ['a {thing}'],
-        'thing' : ['toaster', 'fire truck', 'flower', 'dildo'],
+        'singular_object' : ['a {object}'],
+        'object' : ['toaster', 'fire truck', 'flower', 'dildo'],
+        'bad_thing' : ['Nazi', 'fascist', 'Communist', 'pedophile', 'nymphomaniac'],
+        'weird_thing' : ['zombie', 'alien', 'painter', 'American'],
+
+        'things' : ['{objects}', '{bad_things}', '{weird_things}', '{animals}'],
+        'objects' : ['toasters', 'fire trucks', 'flowers', 'dildos'],
+        'bad_things' : ['Nazis', 'fascists', 'Communists', 'pedophiles', 'nymphomaniacs'],
+        'weird_things' : ['zombies', 'aliens', 'painters', 'Americans'],
+
         'singular_animal' : ['a {animal}'],
-        'animal' : ['tiger', 'gorilla', 'dolphin', 'hippo', 'panda'],
+        'animal' : ['tiger', 'gorilla', 'dolphin', 'hippo', 'panda', 'duck'],
+        'animals' : ['tigers', 'gorillas', 'dolphins', 'hippos', 'pandas', 'ducks'],
 
         'be_state' : ['be {stated}'],
         'change_state' : ['shock', 'impress', 'convince'],
         'stated' : ['shocked', 'impressed', 'happy'],
         'emote' : ['laugh', 'cry'],
-        'bad_thing' : ['Nazi', 'fascist', 'Communist', 'pedophile', 'nymphomaniac'],
-        'weird_thing' : ['zombie', 'alien', 'painter', 'American'],
+
+        'topic' : ['sex', 'cooking', 'driving', 'fashion', 'make-up', 'Dota', 'gaming', 'seduction', 'relationship', 'shopping'],
     }
 }
