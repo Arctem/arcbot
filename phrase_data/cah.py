d6a96bd37ff7ca243c1b959d2c447fb57fe4816c
Russell White <rarctem@gmail.com>
2015-01-15 15:28:26 -0700

    Parse out ending punctuation from some stuff for CAH.

--- phrase_data/cah.py
+++ phrase_data/cah.py
@@ -13,6 +13,8 @@ with open('phrase_data/against-humanity/source/cards.json') as card_file:
     all_cards = json.loads(card_file.read())
     for card in all_cards:
         if card['numAnswers'] is 0:
+            if card['text'][-1] == '.' and card['text'][-2] != '.':
+                card['text'] = card['text'][:-1]
             data['cah']['white_card'].append(card['text'])
         else:
             if card['text'].count('_') != card['numAnswers']\
