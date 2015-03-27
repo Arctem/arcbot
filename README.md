bb8f9df86f7c15fdcb50795010469fd9ebdb1376
Russell White <rarctem@gmail.com>
2015-03-27 18:05:58 -0500

    Make work properly with the new ircbot package.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #Arcbot
 
-This is just a simple chatbot I've written over the last couple years. Its features include poorly organized and documented code, major feature creep, and a lack of regular commits.
+This is just a simple chatbot I've written over the last couple years. Its
+primary features include poorly organized and documented code, major feature
+creep, and a lack of regular commits.
---
+++ phrase_commands.py
@@ -0,0 +1,32 @@
+import imp, phrase_data
+
+from ircbot.command import IRCCommand
+
+import phrase_maker.phrase_maker as phrase_maker
+
+
+def get_phrase_commands():
+    commands = []
+    mods = phrase_data.modules
+
+    for mod_file in mods:
+        if '__init__' in mod_file:
+            continue
+        else:
+            mod = imp.load_source('test', mod_file)
+            phrase_maker.load_module(mod)
+
+            mod_name = mod_file.split('/')[-1].rsplit('.', 1)[0]
+            
+            commands.append(PhraseCommand(mod_name))
+    return commands
+
+
+class PhraseCommand(IRCCommand):
+    def __init__(self, phrase):
+        def fun(self, user, chan, args):
+            response = phrase_maker.make(self.phrase, user)
+            self.owner.send_privmsg(chan, user + ': ' + response)
+
+        IRCCommand.__init__(self, phrase, fun)
+        self.phrase = phrase
